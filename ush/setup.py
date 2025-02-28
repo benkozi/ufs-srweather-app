79c04386bfa2b0e2bee429ae09f4c633e9e97bad
Ben Koziol <bkoziol@redlineperf.com>
2025-02-28 11:39:03 -0700

    test on hera [ci skip]

--- ush/setup.py
+++ ush/setup.py
@@ -1773,7 +1773,7 @@ def setup(USHdir, user_config_fn="config.yaml", debug: bool = False):
             logger.warning("UFS_FIRE is not enabled; setting FIRE_NUM_TASKS = 0")
 
     # Verify correct LEVP specified for FV3_HRRR_gf
-    if expt_config["workflow"]["CCPP_PHYS_SUITE"] == "FV3_HRRR_gf":
+    if expt_config["workflow"].get("CCPP_PHYS_SUITE") == "FV3_HRRR_gf":
         task_names = ("task_make_ics", "task_make_lbcs")
         not_correct_levp = [expt_config[task_name].get("LEVP") != 66 for task_name in task_names]
         if len(not_correct_levp) > 0:
