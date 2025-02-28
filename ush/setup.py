b56e0ceee12af3c092704cfb71a70ebbee09cb57
benkozi <bkoziol@redlineperf.com>
2025-02-28 13:14:15 -0700

    fix: add validation for LEVP (#14)

--- ush/setup.py
+++ ush/setup.py
@@ -1772,6 +1772,14 @@ def setup(USHdir, user_config_fn="config.yaml", debug: bool = False):
         if fire_conf["FIRE_NUM_TASKS"] < 1:
             logger.warning("UFS_FIRE is not enabled; setting FIRE_NUM_TASKS = 0")
 
+    # Verify correct LEVP specified for FV3_HRRR_gf
+    if expt_config["workflow"].get("CCPP_PHYS_SUITE") == "FV3_HRRR_gf":
+        task_names = ("task_make_ics", "task_make_lbcs")
+        not_correct_levp = [task_name for task_name in task_names if expt_config[task_name].get("LEVP") != 66]
+        vcoord_file = "{{ workflow.FIXam }}/global_hyblev_fcst_rrfsL65.txt"
+        if len(not_correct_levp) > 0:
+            raise ValueError(f"Set 'LEVP: 66' and 'VCOORD_FILE: \"{vcoord_file}\"' in task(s) '{not_correct_levp}' when using 'CCPP_PHYS_SUITE: FV3_HRRR_gf'")
+
     #
     # -----------------------------------------------------------------------
     #
