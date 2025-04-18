4ab28e5a02cdca3501737e2e2e6bd3bcec05b176
Ben Koziol <bkoziol@redlineperf.com>
2025-04-18 14:30:09 -0600

    adjust argument

--- scripts/exregional_run_fcst.sh
+++ scripts/exregional_run_fcst.sh
@@ -897,8 +897,8 @@ fi
 #
 $(boolify "${SUB_HOURLY_POST}") != "TRUE"
 
-
-if [ -d "${EXPTDIR}/grid" ] && [ "$(boolify "${HISTORY_NATIVE_GRID}")" = "TRUE" ]; then
+print_info_message "HISTORY_NATIVE_GRID=${HISTORY_NATIVE_GRID}"
+if [ -d "${EXPTDIR}/grid" ] && [ $(boolify "${HISTORY_NATIVE_GRID}") = "TRUE" ]; then
   print_info_message "Creating link to halo0 file"
   create_symlink_to_file "$(find "${EXPTDIR}/grid" -type f -name "C*_grid.tile7.halo0.nc")" \
                          INPUT/grid.tile7.halo0.nc \
