31a4d529d4bc2422be7fa0d39cdb745586c8c967
Ben Koziol <bkoziol@redlineperf.com>
2025-05-16 16:10:44 -0600

    try gc6 run

--- scripts/exregional_make_lbcs.sh
+++ scripts/exregional_make_lbcs.sh
@@ -179,8 +179,9 @@ else
     extrn_mdl_staging_dir="${COMIN}/${EXTRN_MDL_NAME_LBCS}/for_LBCS${SLASH_ENSMEM_SUBDIR}"
     extrn_mdl_var_defns_fp="${extrn_mdl_staging_dir}/${EXTRN_MDL_VAR_DEFNS_FN}.sh"
 fi
+echo "tdk:182: ${EXTRN_MDL_FNS}"
 . ${extrn_mdl_var_defns_fp}
-echo "tdk:181: ${EXTRN_MDL_FNS}"
+echo "tdk:184: ${EXTRN_MDL_FNS}"
 #
 #-----------------------------------------------------------------------
 #
