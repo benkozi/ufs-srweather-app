d2e64d6f365ac7deb175fe827b289d2548313f4f
Ben Koziol <bkoziol@redlineperf.com>
2025-05-21 13:40:51 -0600

    debugging blocked forecast

--- ush/setup.py
+++ ush/setup.py
@@ -1673,6 +1673,7 @@ def setup(ushdir, user_config_fn="config.yaml", debug: bool = False):
         raise ValueError(msg)
 
     if expt_config["workflow"].get("COLDSTART"):
+        import pdb;pdb.set_trace() #tdk:rm
         # If running coupled AQM with cold start, we do not need to get external AQM ICs
         if expt_config.get("cpl_aqm_parm", {}).get("CPL_AQM", False):
             logging.debug("Removing task_aqm_ics_ext due to UFS-AQM cold start")
