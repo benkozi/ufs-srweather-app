3b3c1b915e1a122261f315a8c07f6e0f4e083641
Ben Koziol <bkoziol@redlineperf.com>
2025-05-21 14:17:09 -0600

    debugging blocked forecast

--- ush/setup.py
+++ ush/setup.py
@@ -125,13 +125,6 @@ def load_config_for_setup(ushdir, default_config_path, user_config_path):
     ):
         default_config.update_from(cfg)
 
-    # If running coupled AQM with warm start, we need to get external AQM ICs
-    if not default_config["workflow"]["COLDSTART"] and default_config.get("cpl_aqm_parm", {}).get("CPL_AQM", False):
-        aqm_ics_ext_value = default_config["rocoto"]["tasks"].get("task_aqm_ics_ext")
-        if isinstance(aqm_ics_ext_value, UWYAMLRemove):
-            logging.debug("Enabling task_aqm_ics_ext due to UFS-AQM warm start")
-            default_config["rocoto"]["tasks"].pop("task_aqm_ics_ext")
-
     # Set the path to the top-level ufs-srweather-app directory
     homedir = Path(__file__).parent.parent.resolve()
     default_config["user"]["HOMEdir"] = str(homedir)
@@ -149,6 +142,13 @@ def load_config_for_setup(ushdir, default_config_path, user_config_path):
         tasks = get_yaml_config(homedir / taskgroup)
         keep = {k: v for k, v in tasks.items() if not re.search(r"^default_*", k)}
         default_config["rocoto"]["tasks"].update(keep)
+
+    # If running coupled AQM with warm start, we need to get external AQM ICs
+    if not default_config["workflow"]["COLDSTART"] and default_config.get("cpl_aqm_parm", {}).get("CPL_AQM", False):
+        aqm_ics_ext_value = default_config["rocoto"]["tasks"].get("task_aqm_ics_ext")
+        if isinstance(aqm_ics_ext_value, UWYAMLRemove):
+            logging.debug("Enabling task_aqm_ics_ext due to UFS-AQM warm start")
+            default_config["rocoto"]["tasks"].pop("task_aqm_ics_ext")
 
     # Update one more time in case there are user or machine settings to override the tasks
     for cfg in (machine_config, user_config):
