581ba72817a6a375ab0d74d5d69311f774208b84
Ben Koziol <bkoziol@redlineperf.com>
2025-07-25 11:41:43 -0500

    change path to staged data

--- ush/setup.py
+++ ush/setup.py
@@ -156,9 +156,9 @@ def load_config_for_setup(ushdir, default_config_path, user_config_path):
             default_config.update_from(aqm_coldstart)
         #tdk: remove aqm use case data download task
         if default_config["cpl_aqm_parm"]["USE_AQM_S3_DATA_STAGE"] is True:
-            tdk
             aqm_stage_dst_dir = Path(default_config["cpl_aqm_parm"][
                                          "AQM_STAGE_DST_DIR"]).resolve(strict=True)
+            tdk
 
             task_get_extrn_ics = default_config["task_get_extrn_ics"]["envvars"]
             task_get_extrn_ics["USE_USER_STAGED_EXTRN_FILES"] = True
