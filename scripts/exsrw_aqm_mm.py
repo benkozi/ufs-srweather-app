02cb367aede886bca9bfa2e66a18ebf55b7c006c
Ben Koziol <bkoziol@redlineperf.com>
2025-08-28 12:21:34 -0600

    get python to execute in eval env [ci skip]

--- scripts/exsrw_aqm_mm.py
+++ scripts/exsrw_aqm_mm.py
@@ -1,6 +1,23 @@
 import os
+from pathlib import Path
+
+import typer
+
+
+def main(
+    expt_dir: Path = typer.Option(
+        ...,
+        "--expt-dir", 
+        help="Path to experiment directory",
+        exists=True,
+        readable=True,
+        file_okay=False,
+        dir_okay=True
+    )
+):
+
+    raise ValueError('in exsrw_aqm_mm.py')
+
 
 if __name__ == '__main__':
-    for key, value in os.environ.items():
-        print(f'{key}: {value}')
-    raise ValueError('in exsrw_aqm_mm.py')
+    typer.run(main)
