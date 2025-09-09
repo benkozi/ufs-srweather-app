181290888cf8e737123d6ee5736997d79046136f
Ben Koziol <bkoziol@redlineperf.com>
2025-09-09 11:34:32 -0600

    try with task rename [ci skip]

--- modulefiles/tasks/gaeac6/melodies_monet_run_save_paired.local.lua
+++
@@ -1,4 +0,0 @@
-unload("python_srw")
-load("python_srw_aqm_eval")
-
-prepend_path("PYTHONPATH","/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/NOAA-EPIC/AQM-Eval/src") --tdk: need to install from conda or something
